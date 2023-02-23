8642c275ad35928c0342bed4c96c4b8a0a4006da
jsweger <jsweg13@gmail.com>
2023-02-23 12:04:12 -0500

    Upate switch case

--- MonthNames.java
+++ MonthNames.java
@@ -11,16 +11,47 @@ public class MonthNames {
     public static void main(String[] args) {
         double numericMonth = 0;
         Scanner in = new Scanner(System.in);
-
+        months monthName = months.January;
         System.out.print("Enter your numberic month: ");
         if (in.hasNextDouble()) {
             numericMonth = in.nextDouble();
-            for (months p : months.values())
-                System.out.println("Your weight on " + p + " is " +
-                                    p.getWeight(numericMonth));
+            
+            switch ((int)numericMonth) {
+                case 1: monthName = months.January; break;
+                case 2: monthName = months.February; break;
+                case 3: monthName = months.March; break;
+                case 4: monthName = months.April; break;
+                case 5: monthName = months.May; break;
+                case 6: monthName = months.June; break;
+                case 7: monthName = months.July; break;
+                case 8: monthName = months.August; break;
+                case 9: monthName = months.September; break;
+                case 10: monthName = months.October; break;
+                case 11: monthName = months.November; break;
+                case 12: monthName = months.December; break;
+                default: System.out.println("ERROR - That was not a valid month number!");
+                    System.exit(2);
+            }
+            
         }
         else {
             System.out.println(in.next() + " is not a valid number.");
+            System.exit(1);
+        }
+        
+        switch (monthName){
+            case January: System.out.println("It is January"); break;
+            case February: System.out.println("It is February"); break;
+            case March: System.out.println("It is March"); break;
+            case April: System.out.println("It is April"); break;
+            case May: System.out.println("It is May"); break;
+            case June: System.out.println("It is June"); break;
+            case July: System.out.println("It is July"); break;
+            case August: System.out.println("It is August"); break;
+            case September: System.out.println("It is September"); break;
+            case October: System.out.println("It is October"); break;
+            case November: System.out.println("It is November"); break;
+            case December: System.out.println("It is December"); break;
         }
     }
 }
